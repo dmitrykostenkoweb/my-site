bd668c8df27559e21f5f549c4792d0e1655d795c
Dmitry Kostenko <60621984+dmitrykostenkoweb@users.noreply.github.com>
2022-09-03 23:52:21 +0200

    Added cursor link icons

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,41 @@
 <template>
   <section id="main">
     <h1 class="content name">Dmytro Kostenko</h1>
+    <p class="content post">frontend developer</p>
     <p class="content likes">Hi, I like code, food and sleep.</p>
-    <MyLinks class="links" />
-    <!--    <canvas></canvas>-->
+    <MyLinks class="links" @on-mouse-enter="setLink" />
+    <canvas></canvas>
   </section>
-  <div ref="cursor" class="cursor" />
+  <div ref="cursor" class="cursor">
+    <component class="cursor-icon" :is="generateLinkCursor" />
+  </div>
 </template>
 
 <script setup lang="ts">
-import MyLinks from "@/components/MyLinks";
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { useCustomCursor, useCursorAnimation } from "@/composables";
+import MyLinks from "@/components/MyLinks";
 import EmailIcon from "@/components/icons/EmailIcon.vue";
+import LinkedinIcon from "@/components/icons/LinkedinIcon.vue";
+import type { Links } from "@/models";
 
 const cursor = ref<Element | null>(null);
+const activeLink = ref<string | null>(null);
 
+const setLink = (name: Links | null): void => {
+  activeLink.value = name;
+};
+
+const generateLinkCursor = computed(() => {
+  switch (activeLink.value) {
+    case "gmail":
+      return EmailIcon;
+    case "linkedin":
+      return LinkedinIcon;
+    default:
+      return null;
+  }
+});
 onMounted(async (): Promise<void> => {
   useCursorAnimation();
   if (cursor.value) await useCustomCursor().cursorInit(cursor.value);
@@ -25,6 +45,8 @@ onMounted(async (): Promise<void> => {
 <style lang="scss">
 #main {
   display: flex;
+  align-items: center;
+  justify-content: center;
   width: 100%;
   height: 100%;
 }
@@ -35,17 +57,21 @@ onMounted(async (): Promise<void> => {
   left: var(--base-app-margin);
 }
 
-.likes {
+.post {
   font-size: var(--xl-font-size);
   top: 96px;
   left: var(--base-app-margin);
 }
 
+.likes {
+  font-size: var(--xl-font-size);
+  top: 160px;
+  left: var(--base-app-margin);
+}
+
 .links {
   position: absolute;
-  width: 50px;
-  height: 50px;
-  top: var(--base-app-margin);
-  right: var(--base-app-margin);
+  top: 224px;
+  left: var(--base-app-margin);
 }
 </style>
---
+++ src/models/index.ts
@@ -0,0 +1,1 @@
+export type Links = "gmail" | "linkedin";
